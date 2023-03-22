af1bea2b3324b8c5fe5eca666396f2a5eda6c522
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2023-03-21 21:44:15 -0300

    Update Modelo.py

--- Streamlit_titanic/Modelo.py
+++ Streamlit_titanic/Modelo.py
@@ -96,3 +96,13 @@ sup_clas_df = pd.DataFrame(sup_clas)
 sup_clas_df = sup_clas_df.rename(index={1: 'Alta', 2: 'Media', 3: 'Baja'})
 st.bar_chart(sup_clas_df)
 
+
+st.markdown(''' 
+  A modo de conclusión, los gráficos presentados nos brindan información valiosa sobre la tragedia del Titanic y la composición de sus pasajeros. En primer lugar, se confirma que el protocolo de "Mujeres y niños primero" fue implementado en la evacuación del barco, ya que el 74.68% de los supervivientes fueron mujeres. Además, se puede observar que los niños menores de 10 años tuvieron una mayor tasa de supervivencia.
+
+Por otro lado, la distribución de los supervivientes según la clase social muestra que la clase acomodada fue la que contó con mayor cantidad de supervivientes. Esto se debe en gran parte a que, al ser una clase social de mayor poder adquisitivo, contaba con mayores privilegios y accesos a las balsas salvavidas. La clase media, por otro lado, fue la que tuvo menor cantidad de supervivientes, lo cual podría deberse a su menor representación en el barco y por ende, menor cantidad de botes salvavidas.
+
+En cuanto a la distribución de pasajeros según la clase social, se observa que la clase baja era la más numerosa, lo cual es un hecho relevante ya que contradice la idea inicial de que el Titanic era un barco exclusivo para ricos. 
+
+En conclusión, los gráficos nos brindan información valiosa para entender la tragedia del Titanic y las diferentes variables que influyeron en la tasa de supervivencia de sus pasajeros.
+''')
